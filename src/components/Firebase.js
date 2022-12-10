d20ce9d91d856c6d3034ed7a269c7838b0b8fbd7
Hasnat <webdevhasnat@gmail.com>
2022-12-10 22:00:10 +0600

    done

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -4,12 +4,12 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB2aqau1kP6n3M9oLyc7O2TUCD0dkFqF0M",
+  apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
   authDomain: "blogz-9530b.firebaseapp.com",
   projectId: "blogz-9530b",
   storageBucket: "blogz-9530b.appspot.com",
-  messagingSenderId: "1023445072338",
-  appId: "1:1023445072338:web:77c382f666cae948bc300b",
+  messagingSenderId: `${process.env.REACT_APP_MESSAGINGSENDER_ID}`,
+  appId: `${process.env.REACT_APP_FIREBASE_APP_ID}`,
 };
 
 const app = initializeApp(firebaseConfig);
